b4b3663c4b047fbe3992726d0554fcbfc7d940d1
Kadaiah <arun.kumark@live.com>
2018-07-12 06:28:53 -0700

    this is my commit

---
+++ word/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+#def home(request):
+#    return HttpResponse('home')
+
+#def eggs(request):
+#    return HttpResponse('eggs are great')
+def home(request):
+    return render(request,'home.html')
+def count(request):
+    fulltext =request.GET['fulltext']
+    wordlist =fulltext.split()
+    worddictonary={}
+
+    for word in wordlist:
+        if word in worddictonary:
+            worddictonary[word] +=1
+        else:
+            worddictonary[word]=1
+
+    return render(request,'count.html',{"fulltext":fulltext,'count':len(wordlist),'worddictonary':worddictonary})
+def about(request):
+    return render(request,'about.html')
